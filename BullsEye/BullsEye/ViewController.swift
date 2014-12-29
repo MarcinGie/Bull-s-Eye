5c23469eb011ac35cbc772268d395837265531b2
Marcin Grześkowiak <marcin.grzeskowiak@codete.co>
2014-12-29 17:29:38 +0100

    "Hit me!" button generates random target number

--- BullsEye/BullsEye/ViewController.swift
+++ BullsEye/BullsEye/ViewController.swift
@@ -10,16 +10,44 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var slider: UISlider!
+    
+    var currentValue: Int = 0
+    var targetValue: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        startNewRound()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func startNewRound() {
+        targetValue = 1 + Int(arc4random_uniform(100))
+        currentValue = 50
+        slider.value = Float(currentValue)
+    }
+    
+    @IBAction func showAlert() {
+        let message = "The value of the slider is: \(currentValue)"+"\nThe target value is: \(targetValue)"
+        
+        let alert = UIAlertController(title: "Hello world", message: message, preferredStyle: .Alert)
+        
+        let action = UIAlertAction(title: "OK", style: .Default, handler: nil)
+        
+        alert.addAction(action)
+        
+        presentViewController(alert, animated: true, completion: nil)
+    
+        startNewRound()
+    }
+    
+    @IBAction func sliderMoved(slider: UISlider) {
+        println("The value changed and is now at: \(slider.value)")
+        currentValue = lroundf(slider.value)
+    }
 }
 
