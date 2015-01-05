72dad59aaa5ed7b27cd482b5a46cfaa5a6a873c8
Marcin Grześkowiak <marcingrzeskowiak@MacBook-Pro-Marcin.local>
2015-01-05 01:48:55 +0100

    Game finished. Adding images started. 118

---
+++ BullsEye/BullsEye/AboutViewController.swift
@@ -0,0 +1,16 @@
+//
+//  AboutViewController.swift
+//  BullsEye
+//
+//  Created by Marcin Grześkowiak on 05.01.2015.
+//  Copyright (c) 2015 Marcin. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+
+    @IBAction func close() {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+}
--- BullsEye/BullsEye/ViewController.swift
+++ BullsEye/BullsEye/ViewController.swift
@@ -68,12 +68,20 @@ class ViewController: UIViewController {
         
         let message = "You scored \(points) points"
         let alert = UIAlertController(title: title, message: message, preferredStyle: .Alert)
-        let action = UIAlertAction(title: "OK", style: .Default, handler: nil)
+        let action = UIAlertAction(title: "OK", style: .Default, handler: {
+            action in self.startNewRound()
+            self.updateLabels()
+        })
         
         alert.addAction(action)
         
         presentViewController(alert, animated: true, completion: nil)
+
+    }
     
+    @IBAction func startOverTapped() {
+        score = 0
+        round = 0
         startNewRound()
         updateLabels()
     }
